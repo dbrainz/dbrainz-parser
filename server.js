7283eb247cc70cbe41655af39bdf3cbb7d848db4
Darren Brain <darren.brain@gmail.com>
2017-04-03 19:46:58 +0000

    initial test

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 var express = require('express');
+
+var app = express();
+
+
+app.get('/api/whoami/', (req, res) => {
+    console.log(req.header('x-forwarded-for') || req.connection.remoteAddress);
+    var langs = req.acceptsLanguages();
+    console.log(langs[0]);
+ //   console.log(req.headers['user-agent']);
+ console.log(req.headers)
+ console.log(req.headers['user-agent'])
+})
+
+app.listen(process.env.PORT || 3000 || 8080);
