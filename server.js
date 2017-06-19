c59f5fcba152b15c339c237c7de595b0241fa6fa
Darren Brain <darren.brain@gmail.com>
2017-06-19 19:42:42 +0000

    added message to all invalid paths

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// FCC Request Header Parser Microservice
+// https://www.freecodecamp.com/challenges/request-header-parser-microservice
+// Returns the IP, preferred language and the operating system in JSON format
+// to any request to path /api/whoami
+
+
 var express = require('express');
 
 var app = express();
-
 
 app.get('/api/whoami/', (req, res) => {
     var reqIP = (req.header('x-forwarded-for') || req.connection.remoteAddress);
@@ -9,6 +14,10 @@ app.get('/api/whoami/', (req, res) => {
     var operSys =  req.headers['user-agent'].substr(req.headers['user-agent'].indexOf('(') + 1, req.headers['user-agent'].indexOf(')') - req.headers['user-agent'].indexOf('(') - 1)
     var returnObj = {'ip' : reqIP, 'language' : langs[0], 'browser' : operSys}
     res.end(JSON.stringify(returnObj));
+})
+
+app.get('/*', (req,res) => {
+    res.end('You must use the path /api/whoami');
 })
 
 app.listen(process.env.PORT || 3000 || 8080);
